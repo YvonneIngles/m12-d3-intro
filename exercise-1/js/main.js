feff6bdef58c5c08bdc5a7433aa00f8a1d15be03
Yvonne Ingles <ydingles@gmail.com>
2017-04-11 11:08:54 -0700

    exercise 1

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -1,16 +1,27 @@
 // Main JavaScript File
 
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
-$(function() {
+$(function () {
 
   // Select the element with id `sandbox` and append a `div` element to it. Make sure to store the div in a JavaScript varaible
-
+  var div = d3.select('#sandbox')
+    .append('div')
+    .attr('id', 'my-div')
+    .style('fill', 'black');
   // Create a variable `mySvg` by appending an `svg` element to your newly created `div`
-
+  var mySvg = div.append('svg');
   // Set both the width and height attributes of your `svg` to 300 
-
+  mySvg = {
+    witdth: 300,
+    height: 300,
+  };
   // Append a `circle` element to your `svg`, setting the properties noted in index.html
+  mySvg.attr('height', 300)
+    .attr('width', 300);
 
   // Append a `rect` element to your `svg`, setting the properties noted in index.html
-
+  mySvg.append('circle')
+    .attr('r', 10)
+    .attr('cx', 100)
+    .attr('cy', 100);
 });
